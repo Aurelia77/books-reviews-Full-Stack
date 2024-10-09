1ccca5acf82355f6a7253a6b39b09dee81ef7536
Aurelia H <aurelia.h@hotmail.fr>
2024-10-09 17:55:37 +0200

    feature: structure swr variables etc...

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -25,7 +25,7 @@ const NavBar = (): JSX.Element => {
   const location = useLocation();
 
   const { user } = useUserStore();
-  console.log("USER", user);
+  //console.log("USER", user);
 
   return (
     <div className="sticky top-0 z-20 flex h-12 items-center bg-primary/70 p-1 text-muted shadow-md">
@@ -82,47 +82,50 @@ const NavBar = (): JSX.Element => {
               </NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
-          <div className="flex gap-1">
+          {user ? (
+            <div className="flex gap-1">
+              <NavigationMenuItem>
+                <Link to="/account">
+                  <NavigationMenuLink asChild>
+                    <span
+                      className={cn(
+                        navigationMenuTriggerStyle(),
+                        location.pathname === "/account" &&
+                          "bg-primary/90 text-foreground"
+                      )}
+                    >
+                      <CircleUserRound />
+                    </span>
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link to="/" onClick={signoutFirebase}>
+                  <NavigationMenuLink asChild>
+                    <span className={navigationMenuTriggerStyle()}>
+                      <X />
+                    </span>
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </div>
+          ) : (
             <NavigationMenuItem>
-              <Link to="/account">
+              <Link to="/login">
                 <NavigationMenuLink asChild>
                   <span
                     className={cn(
                       navigationMenuTriggerStyle(),
-                      location.pathname === "/account" &&
-                        "bg-primary/90 text-foreground"
+                      location.pathname === "/login" &&
+                        "bg-primary/90 text-foreground" // Ajout de classes conditionnelles pour l'élément actif
                     )}
                   >
-                    <CircleUserRound />
+                    <LogIn />
                   </span>
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link to="/" onClick={signoutFirebase}>
-                <NavigationMenuLink asChild>
-                  <span className={navigationMenuTriggerStyle()}>
-                    <X />
-                  </span>
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </div>
-          <NavigationMenuItem>
-            <Link to="/login">
-              <NavigationMenuLink asChild>
-                <span
-                  className={cn(
-                    navigationMenuTriggerStyle(),
-                    location.pathname === "/login" &&
-                      "bg-primary/90 text-foreground" // Ajout de classes conditionnelles pour l'élément actif
-                  )}
-                >
-                  <LogIn />
-                </span>
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
+          )}
         </NavigationMenuList>
       </NavigationMenu>
       <DarkModeToggle />
--- src/pages/MyReadBooksPage.tsx
+++ src/pages/MyReadBooksPage.tsx
@@ -1,78 +1,95 @@
 import BookInfos from "@/components/BookInfos";
+import BookSkeleton from "@/components/BookSkeleton";
 import CustomLinkButton from "@/components/CustomLinkButton";
 import FeedbackMessage from "@/components/FeedbackMessage";
 import Title from "@/components/Title";
 import { getDocsByQueryFirebase } from "@/firebase";
+import useUserStore from "@/hooks/useUserStore";
 import { UserType } from "@/types";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const MyReadBooksPage = (): JSX.Element => {
-  const [myReadBooksIds, setMyReadBooksIds] = useState<string[] | undefined>(
-    []
-  );
+  const { user } = useUserStore();
+  const fetcher = (userId: string | null) => {
+    if (userId)
+      return getDocsByQueryFirebase<UserType>("users", "id", userId).then(
+        (users) => {
+          console.log("USERS", users);
+          return users[0].booksRead;
+        }
+        //setMyReadBooksIds(users[0].booksRead)
+      );
+    //si userId est null, on retourne une promesse vide
+    return Promise.resolve([]);
+    //   getDocsByQueryFirebase("books", "bookId", bookId).then((books) => {
+    //   setMyReadBooks((prevBooks) => [...prevBooks, ...books]);
+    // });
+  };
 
-  // const fetcher = (bookId: string) => {
-  //   getDocsByQueryFirebase("books", "bookId", bookId).then((books) => {
-  //     setMyReadBooks((prevBooks) => [...prevBooks, ...books]);
-  //   });
-  // };
+  const {
+    data: myReadBooksIds,
+    error,
+    isLoading,
+  } = useSWR<string[]>(user ? user.uid : null, fetcher);
 
-  // const {
-  //   data: booksFromAPI,
-  //   error,
-  //   isLoading,
-  // } = useSWR<BookType[]>(searchUrl, fetcher);
+  console.log("isLoading", isLoading);
+  console.log("myReadBooksIds", myReadBooksIds);
 
-  useEffect(() => {
-    getDocsByQueryFirebase<UserType>(
-      "users",
-      "id",
-      "YGZ3EAw9lFdGaCewuQhxyrqcFi42"
-    ).then((users) => setMyReadBooksIds(users[0].booksRead));
-  }, []);
-
-  console.log("myReadBooks", myReadBooksIds);
+  const message = `Un problème est survenu dans la récupération de vos livres lus => ${error?.message}`;
 
   return (
     <div className="h-full min-h-screen">
       <Title>Mes livres lus</Title>
-      {myReadBooksIds && myReadBooksIds.length > 0 ? (
-        <ul className="flex h-full flex-col pb-16">
-          {myReadBooksIds.map(
-            (bookId) =>
-              bookId && (
-                // Ici on passe le bookId en props (et pas le book complet comme dans BooksSearchPage)
-                <BookInfos
-                  key={bookId}
-                  bookId={bookId}
-                  //friendsWhoReadBook={["Loulou"]}
-                />
-              )
-          )}
-        </ul>
-      ) : (
+
+      {isLoading ? (
         <div>
-          <FeedbackMessage message="Aucun livre pour l'instant" />
-          <div className="flex flex-col gap-4 py-12">
-            <p className="ml-1">Essayez d'aller par là !</p>
-            <CustomLinkButton
-              className="bg-accent/60"
-              linkTo="/mybooks/searchbook"
-            >
-              Recherche de livre
-            </CustomLinkButton>
-            <CustomLinkButton className="bg-secondary/80">
-              Livres de mes amis
-            </CustomLinkButton>
-            <CustomLinkButton className="bg-primary/50">
-              Suggestions
-            </CustomLinkButton>
-            {/* <FeedbackMessage
+          <BookSkeleton />
+          <BookSkeleton />
+          <BookSkeleton />
+        </div>
+      ) : error ? (
+        <FeedbackMessage message={message} type="error" />
+      ) : myReadBooksIds ? (
+        myReadBooksIds.length > 0 ? (
+          <ul className="flex h-full flex-col pb-16">
+            {myReadBooksIds.map(
+              (bookId) =>
+                bookId && (
+                  // Ici on passe le bookId en props (et pas le book complet comme dans BooksSearchPage)
+                  <BookInfos
+                    key={bookId}
+                    bookId={bookId}
+                    //friendsWhoReadBook={["Loulou"]}
+                  />
+                )
+            )}
+          </ul>
+        ) : (
+          <div>
+            <FeedbackMessage message="Aucun livre pour l'instant" />
+            <div className="flex flex-col gap-4 py-12">
+              <p className="ml-1">Essayez d'aller par là !</p>
+              <CustomLinkButton
+                className="bg-accent/60"
+                linkTo="/mybooks/searchbook"
+              >
+                Recherche de livre
+              </CustomLinkButton>
+              <CustomLinkButton className="bg-secondary/80">
+                Livres de mes amis
+              </CustomLinkButton>
+              <CustomLinkButton className="bg-primary/50">
+                Suggestions
+              </CustomLinkButton>
+              {/* <FeedbackMessage
             message="Vous n'avez pas encore lu de livre."
             type="error"
           /> */}
+            </div>
           </div>
-        </div>
+        )
+      ) : (
+        ""
       )}
     </div>
   );
