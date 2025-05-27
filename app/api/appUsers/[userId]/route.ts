86d55752bc2120f587037f6560bc097fdf55b445
Aurelia H. <aurelia.h@hotmail.fr>
2025-05-27 17:28:43 +0200

    fix: resolve GET export Vercel deploiement error

--- app/api/appUsers/[userId]/route.ts
+++ app/api/appUsers/[userId]/route.ts
@@ -1,8 +1,8 @@
 import { prisma } from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(
-  req: NextRequest,
+  req: Request,
   { params }: { params: { userId: string } }
 ) {
   const { userId } = params;
